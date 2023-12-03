7968cd7f3422741c5f6022f3f6c28a0c4e170d81
kyonav <sobbing_rat@protonmail.com>
2023-12-03 11:26:37 -0300

    first commit

---
+++ dwm/scripts/autostart.sh
@@ -0,0 +1,11 @@
+#! /bin/sh
+
+# keyboard layout
+setxkbmap -layout br -variant abnt2 &
+
+# background wallpaper
+nitrogen --set-auto ~/pics/wpps/wallhaven-2y6kd9.png &
+
+# window compositor
+xcompmgr &
+
