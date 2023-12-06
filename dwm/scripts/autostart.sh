1544ad6798dd4185e9dce59bc5f1f93809c2e4ed
kyonav <sobbing_rat@protonmail.com>
2023-12-06 11:06:30 -0300

    autostart, shell, st and zsh changes

--- dwm/scripts/autostart.sh
+++ dwm/scripts/autostart.sh
@@ -4,7 +4,8 @@
 setxkbmap -layout br -variant abnt2 &
 
 # background wallpaper
-nitrogen --set-auto ~/pics/wpps/wallhaven-2y6kd9.png &
+#nitrogen --set-auto ~/pics/wpps/wallhaven-2y6kd9.png &
+feh --bg-scale ~/pics/wpps/wallhaven-1p26mw.png &
 
 # window compositor
 xcompmgr &
